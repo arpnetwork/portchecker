fdffc4167fb410fb85b747d61066ad09de1b0ca1
rdspot <rdspot@arpnetwork.org>
2018-08-28 17:50:36 +0800

    Format code

--- src/checker.c
+++ src/checker.c
@@ -26,28 +26,34 @@
 
 void print_usage_and_exit(char *progname);
 
-int main(int argc, char *argv[]) {
-    if (argc < 2 || argc > 3) {
+int main(int argc, char *argv[])
+{
+    if (argc < 2 || argc > 3)
+    {
         print_usage_and_exit(argv[0]);
     }
 
     struct sockaddr_in addr;
     addr.sin_family = AF_INET;
 
-    if (argc == 3) {
-        if (inet_pton(AF_INET, argv[1], &addr.sin_addr) != 1) {
+    if (argc == 3)
+    {
+        if (inet_pton(AF_INET, argv[1], &addr.sin_addr) != 1)
+        {
             print_usage_and_exit(argv[0]);
         }
     }
     int port = atoi(argv[argc - 1]);
-    if (port <= 0 || port > 65535) {
+    if (port <= 0 || port > 65535)
+    {
         print_usage_and_exit(argv[0]);
     }
     addr.sin_port = htons(port);
 
-    int sockfd = socket(AF_INET, SOCK_STREAM, 0); assert(sockfd >= 0);
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    assert(sockfd >= 0);
     fcntl(sockfd, F_SETFL, O_NONBLOCK);
-    connect(sockfd, (struct sockaddr *) &addr, sizeof (addr));
+    connect(sockfd, (struct sockaddr *)&addr, sizeof(addr));
 
     fd_set fdset;
     struct timeval tv;
@@ -57,23 +63,28 @@ int main(int argc, char *argv[]) {
     tv.tv_usec = 0;
 
     int rc = -1;
-    if (select(sockfd + 1, NULL, &fdset, NULL, &tv) == 1) {
+    if (select(sockfd + 1, NULL, &fdset, NULL, &tv) == 1)
+    {
         socklen_t len = sizeof rc;
         getsockopt(sockfd, SOL_SOCKET, SO_ERROR, &rc, &len);
     }
 
     close(sockfd);
 
-    if (rc == 0) {
+    if (rc == 0)
+    {
         printf("ok\n");
-    } else {
+    }
+    else
+    {
         printf("failed\n");
     }
 
     return rc;
 }
 
-void print_usage_and_exit(char *progname) {
+void print_usage_and_exit(char *progname)
+{
     fprintf(stderr, "Usage: %s [IP] PORT\n", progname);
     exit(-1);
 }
